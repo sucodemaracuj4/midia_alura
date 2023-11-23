16bca5aa42ee92a08c571a2e75a8e96fa7575ef9
su <111879114+sucodemaracuj4@users.noreply.github.com>
2023-11-23 11:01:22 -0300

    Update main.js

--- images/main.js
+++ images/main.js
@@ -1,15 +1,25 @@
-function tocaSom(idElementAudio){
-    document.querySelector('idElementAudio').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
+ 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-document.querySelector('.tecla_pom').onclick = tocaSom;
-let contador = 0
-while(contador <listaDeTeclas.length) {
-    const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclick = function();
-    {tocaSom(idAudio);
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
-    contador +=
-    console.log(contador);
+
+    tecla.onkeydown = function (){
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
